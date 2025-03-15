e5c8dc56e33981c4150acdfc253001f85b11d47f
LooooZ <162182067+WhCreating@users.noreply.github.com>
2025-03-15 22:34:58 +0500

    Update log.py

--- logs/log.py
+++ logs/log.py
@@ -1,1 +1,10 @@
+import logging.config
+import yaml
+
+def logs():
+
+    with open('logs/config.yaml', 'rt') as f:
+        conf = yaml.safe_load(f.read())
+
+        logging.config.dictConfig(conf)
 
