2f9c5a1f2b264bca72d08252e278912b3e65da13
LooooZ <162182067+WhCreating@users.noreply.github.com>
2025-03-15 22:34:05 +0500

    Update filters.py

--- logs/filters.py
+++ logs/filters.py
@@ -1,1 +1,20 @@
+import logging
+import colorama
 
+class ColorForm(logging.Formatter):
+    COLORS = {
+        logging.DEBUG: colorama.Fore.BLUE,
+        logging.INFO: colorama.Fore.GREEN,
+        logging.WARNING: colorama.Fore.YELLOW,
+        logging.ERROR: colorama.Fore.RED,
+        logging.CRITICAL: colorama.Fore.RED + colorama.Style.BRIGHT,
+
+    }
+
+    def format(self, record):
+        if record.levelno in self.COLORS:
+            record.levelname = (f"{self.COLORS[record.levelno]}"
+                                f"{record.levelname}{colorama.Style.RESET_ALL}")
+            record.msg = (f"{self.COLORS[record.levelno]}"
+                          f"{record.msg}{colorama.Style.RESET_ALL}")
+        return super().format(record)
